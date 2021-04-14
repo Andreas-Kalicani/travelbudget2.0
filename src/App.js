31c8859b1efb3468018283bfe0d69f82de7135fd
unknown <sergio-caballero_student2021@wilder.school>
2021-04-14 16:54:15 +0200

    json yarn changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,5 @@
 import React from "react"; 
 import './App.css';
-<<<<<<< HEAD
-import Navbar from './componets/navigation/navbar';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
-import { MdDashboard } from '../node_modules/react-icons/md';
-import Dashboard from "./pages/Dashboard";
-
-
-function App() {
-  return (
-  <>
-  <Router>
-   < Navbar />
-   <Switch>
-     <Route path="/" exact />
-     <Route path="/dashboard" component={Dashboard} />
-     </Switch>
-   </Router>
-    </>
-=======
 import Navbar from './components/navigation/navbar';
 import {Switch, Route} from "react-router-dom"
 import InputForm from "./components/form/InputForm";
@@ -37,7 +18,6 @@ function App() {
         />  
       </Switch> 
     </div>
->>>>>>> 64813632655c07769842ef89621716e98195a944
   );
 }
 
