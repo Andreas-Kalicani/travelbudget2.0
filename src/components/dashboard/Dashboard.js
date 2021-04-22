306165b044867f0baed62fc740a1e13968c5a56f
Javier Gonzalez <jgromeo93@gmail.com>
2021-04-22 09:41:12 +0200

    lifted state up on dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import AddComponent from './add-component/AddComponent'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Charts from './add-component/charts';
-import Chart2 from './chart2';
+import Charts from './charts-components/charts';
+import Chart2 from './charts-components/chart2';
 import "./dashboard.css"; 
 import { AppProvider } from '../../context/AppContext';
 
